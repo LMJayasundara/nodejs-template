e4dfecc15051ef51e0e282e25f4cc8e937ff7d8d
LMJayasundara <jmlahirujayasundara@gmail.com>
2020-12-04 17:08:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+var app = express();
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(express.static('public'));
+
+app.listen(app.get('port'), function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Running on port: ' + app.get('port')); }
+});
