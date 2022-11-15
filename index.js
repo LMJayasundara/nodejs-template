e15c5600cf24a855161ac2f52dc8046bd697a3b0
LMJayasundara <47645312+LMJayasundara@users.noreply.github.com>
2022-11-15 09:50:59 +0530

    Express-Socket.io

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 app.set('port', (process.env.PORT || 8080));
 
 app.use(express.static('public'));
 
-app.listen(app.get('port'), function(err) {
+io.on('connection', (socket) => {
+  console.log('a new client connected');
+  socket.emit('chat', "message from server");
+});
+
+server.listen(app.get('port'), function(err) {
   if (err) {
     console.log(err);
   } else {
---
+++ public/scripts.js
@@ -0,0 +1,5 @@
+const socket = io();
+
+socket.on('chat', message => {
+    console.log('From server: ', message)
+});
